0095a9d4c93b2260038851fb4cc90eace4c326a7
gd-harco <gd-harco@student.42lyon.fr>
2024-07-22 14:37:30 +0200

    manually recreated commit 758f4ff

--- front-end/source/front-end/components/Register.js
+++ front-end/source/front-end/components/Register.js
@@ -1,17 +1,13 @@
 import changeRoute from "../utils/changeRoute.js";
 import {lang} from "../utils/getAllLang";
 import getCookie from "../utils/getCookie";
-
 export default class Register extends HTMLElement {
 	constructor() {
 		super();
 	}
-
 	async connectedCallback() {
-
 		if (!getCookie("lang"))
 			document.cookie = "lang=en";
-
 		let title = await lang.register_page.title[getCookie("lang")];
 		let login_field = await lang.register_page.login_field[getCookie("lang")];
 		let password_field = await lang.register_page.password_field[getCookie("lang")];
@@ -23,15 +19,11 @@ export default class Register extends HTMLElement {
 		let password_too_short = await lang.register_page.password_too_short[getCookie("lang")];
 		let unexpected_error = await lang.register_page.unexpected_error[getCookie("lang")];
 		let invalid_char = await lang.register_page.invalid_char[getCookie("lang")];
-
 		this.innerHTML = `
         <link rel="stylesheet" href="/styles/register.css">
-
         <div id="Register-Element">
-
         <h1 id="title">OUR TRANSCENDENCE</h1>
         <h2 id="form-title">${title}</h2>
-
             <div>
                 <form id="register-form" class="form">
                     <div class="">
@@ -48,9 +40,9 @@ export default class Register extends HTMLElement {
                             <img src="/imgs/viewOff.svg" alt="" id="img_see" height="24px" width="24px">
                             </button>
                         </div>
-
                     <div id="button-div">
                         <button type="submit" id="submit-register" class="submit-button">${register_button}</button>
+						<img src="/imgs/loading.gif" style="width:48px;height:48px;" id="loading">
                     </div>
             		<p id="message"></p>
                     <div id="red-line"></div>
@@ -65,24 +57,27 @@ export default class Register extends HTMLElement {
 		const pass_toggle = this.querySelector("#togglePassword");
 		const submitButton = this.querySelector("#submit-register");
 		const message = document.getElementById('message');
+		const loading = document.querySelector("#loading");
+
+		loading.style.display = "none";
 
 		pass_toggle.addEventListener("click", (e) => {
 			e.preventDefault();
-
 			if (pass.type === "password")
 				pass.type = "text";
 			else
 				pass.type = "password";
 		});
-
 		function onlyAlphanumeric(str) {
 			return /^[a-zA-Z0-9_-]{5,15}$/.test(str);
 		}
-
 		form.addEventListener("submit", (e) => {
 			e.preventDefault();
 			const login_value = login.value;
 			const pass_value = pass.value;
+			submitButton.style.display = "none";
+			loading.style.display = "block";
+
 
 			if (login_value.trim() === '' || pass_value.trim() === '') {
 				message.innerText = complete_fields;
@@ -91,6 +86,8 @@ export default class Register extends HTMLElement {
 				message.style.marginTop = "10px";
 				message.style.marginLeft = "20px";
 				message.style.maxWidth = "300px";
+				submitButton.style.display = "block";
+				loading.style.display = "none";
 				return ;
 			}
 			if (!onlyAlphanumeric(login_value) || !onlyAlphanumeric(pass_value))
@@ -101,6 +98,8 @@ export default class Register extends HTMLElement {
 				message.style.marginTop = "10px";
 				message.style.marginLeft = "20px";
 				message.style.maxWidth = "300px";
+				submitButton.style.display = "block";
+				loading.style.display = "none";
 				return ;
 			}
 
@@ -112,7 +111,6 @@ export default class Register extends HTMLElement {
 				password: pass_value,
 				display_name: login_value,
 			});
-
 			submitButton.disabled = true;
 			fetch(`https://${location.hostname}:4444/register/`, {
 				method: 'POST',
@@ -151,6 +149,8 @@ export default class Register extends HTMLElement {
 					message.style.marginLeft = "20px";
 					message.style.maxWidth = "300px";
 				}
+				loading.style.display = "none";
+				submitButton.style.display = "block";
 				submitButton.disabled = false;
 			}).catch(err => {
 				message.innerText = unexpected_error;
@@ -159,6 +159,8 @@ export default class Register extends HTMLElement {
 				message.style.marginTop = "10px";
 				message.style.marginLeft = "20px";
 				message.style.maxWidth = "300px";
+				loading.style.display = "none";
+				submitButton.style.display = "block";
 				submitButton.disabled = false;
 			})
 		});
